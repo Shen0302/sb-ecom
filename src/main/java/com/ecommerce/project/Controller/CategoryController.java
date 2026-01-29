eda2eda96eab06b6ab75594770076d28ef5c8eb3
Owen <yujingshen03@gmail.com>
2026-01-29 21:32:07 +0000

    Delete Category

--- src/main/java/com/ecommerce/project/Controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/Controller/CategoryController.java
@@ -2,21 +2,16 @@ package com.ecommerce.project.Controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
+    @Autowired
     private CategoryService categoryService;
-
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
@@ -27,6 +22,12 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 
 
